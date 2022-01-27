c7ce59181c8b96c2b4446f89b21eecc95bf18280
ktbcode <kteduca@gmail.com>
2022-01-27 11:17:59 -0500

    delete row on shopping cart

--- js/shoppingcart.js
+++ js/shoppingcart.js
@@ -1,11 +1,15 @@
 
-//select the remove button
-let removeButton = document.querySelector('.bi-dash-circle');
-let table = document.querySelector('.table');
+//Trying to delete table rows when click on the - button but fail 
 
-let selectRows = document.querySelector('.table').getElementsByTagName('tr');
-let rowCount = selectRows.length;
-console.log(rowCount);
+
+//select the remove button
+//let removeButton = document.getElementsByClassName("bi-dash-circle")[1];
+// let removeButton = document.querySelectorAll(".bi-dash-circle");
+// let table = document.querySelector('.table');
+
+// let selectRows = document.querySelectorAll('.table-row');
+// let rowCount = selectRows.length;
+// console.log(rowCount);
 
 // //when clicking the button, delete all row
 
@@ -17,7 +21,22 @@ console.log(rowCount);
 // }
 
 //when click anywhere on the table, it delete the second row
-table.onclick = () =>{
-    table.deleteRow(1);
+// for (let i = 0; i < removeButton.length; i++){
+    
+//     removeButton[i].onclick = () =>{
+//         table.deleteRow(1);
+//     }   
+//     console.log(i);
+
+// }
+
+
+
+//Code is inspired by https://stackoverflow.com/questions/6473111/add-delete-table-rows-dynamically-using-javascript
+function deleteRow(row) {
+    var rowIndex = row.parentNode.parentNode.rowIndex;
+    document.querySelector('.table').deleteRow(rowIndex);
 }
+  
+  
 
